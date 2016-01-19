7d62ab8fc4735f91195ec66192d1d262b9e4313e
Christopher <kung_cool_yes@hotmail.com>
2016-01-19 10:52:21 +0100

    LABB 2

---
+++ Labb 2/Labb_2_Service_och_tjänsten/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+namespace Labb_2_Service_och_tjänsten
+{
+    class Program
+    {
+        [ServiceContract(Namespace = "http://EcUtbildning.Labb_2_Service_och_tjänsten")]
+        public interface IBMICalculator
+        {
+            [OperationContract]// Måste finnas för varje medtod för att det ska följa med i WCF kontraktet
+            int BMICalc(int BMICalc);
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
