ff178e224b60d06ca9c55fb17ec548a84959e328
Christopher <kung_cool_yes@hotmail.com>
2016-01-20 14:39:41 +0100

    Labb 1.2 klar

---
+++ WCF-Labbar/Labberation/Labb_1.2/AdditionService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="AdditionService.asmx.cs" Class="Labb_1._2.AdditionService" %>
---
+++ WCF-Labbar/Labberation/Labb_1.2/Index.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Labb_1._2.AdditionServiceTwoNumbers;
+namespace Labb_1._2
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void AddNumbers_Click(object sender, EventArgs e)
+        {
+            var client = new AdditionServiceTwoNumbers.AdditionServiceSoapClient();
+            Label2.Text = client.AddTwoNumbers(int.Parse(TextBox1.Text), int.Parse(TextBox2.Text));
+        }
+    }
+}
