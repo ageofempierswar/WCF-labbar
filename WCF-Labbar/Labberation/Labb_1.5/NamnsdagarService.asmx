a5f78dad7d3c0df32703a18d6101762e218364ed
Christopher <kung_cool_yes@hotmail.com>
2016-01-27 12:09:57 +0100

    La till del 5 och 6 fick problem med 5 ska ta up det med läraren

---
+++ WCF-Labbar/Labberation/Labb_1.5/NamnsdagarService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="NamnsdagarService.asmx.cs" Class="Labb_1._5.NamnsdagarService" %>
---
+++ WCF-Labbar/Labberation/Labb_1.5/NamnsdagarService.asmx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.IO;
+namespace Labb_1._5
+{
+    /// <summary>
+    /// Summary description for NamnsdagarService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class NamnsdagarService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string GetNameForDate(string input)
+        {
+            var array = File.ReadAllLines(@"..\Namnsdagar.txt");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Contains(input))
+                {
+                    var newArray = array[i].Split(' ');
+                    return input + " har namnsdag " + newArray[0] + "/" + newArray[1];
+                }
+            }
+
+            return array[0];
+        }
+    }
+}
