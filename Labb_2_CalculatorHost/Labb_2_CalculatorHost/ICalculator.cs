6a2f86d61e20d4d9f0a43ca676dd1e824d6d85ac
Christopher <kung_cool_yes@hotmail.com>
2016-01-28 10:15:45 +0100

    LAbb 2 startad

---
+++ Labb_2_CalculatorHost/Labb_2_CalculatorHost/ICalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+namespace Labb_2_CalculatorHost
+{
+    [ServiceContract(Namespace = "http://Christopher.CalculatorHost")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double a, double b);
+        [OperationContract]
+        double Subtract(double a, double b);
+        [OperationContract]
+        double Multiply(double a, double b);
+        [OperationContract]
+        double Divide(double a, double b);
+    }
+}
